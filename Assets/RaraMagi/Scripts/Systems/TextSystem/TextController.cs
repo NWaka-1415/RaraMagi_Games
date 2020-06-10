2d7290b5f4f6617c9bf0df561ebc82f093e5fc24
NWaka <yuuki.ohta.chinta.p@gmail.com>
2020-06-11 04:25:08 +0900

    :+1: update:分岐スキップ対応準備

--- Assets/RaraMagi/Scripts/Systems/TextSystem/TextController.cs
+++ Assets/RaraMagi/Scripts/Systems/TextSystem/TextController.cs
@@ -10,7 +10,7 @@ namespace RaraMagi.Systems.TextSystem
         private float intervalForCharDisplay = 0.05f; // 1文字の表示にかける時間
 
         private int _currentLineIndex = 0; //現在表示している文章番号
-        private string _currentLine = string.Empty; // 現在の文字列
+        private ScenarioData _currentScenario = null; // 現在のシナリオデータ
         private float _timeUntilDisplay = 0; // 表示にかかる時間
         private float _timeBeganDisplay = 1; // 文字列の表示を開始した時間
         private int _lastUpdateCharCount = -1; // 表示中の文字数
@@ -47,7 +47,7 @@ namespace RaraMagi.Systems.TextSystem
                 //最後の文章ではない & ボタンが押された
                 if (_currentLineIndex < _scenarioDataList.Count && isPush)
                 {
-                    SetNextLine();
+                    if (!_currentScenario.IsBranchChoices) SetNextLine();
                 }
                 // 初期に戻る
                 else if (_currentLineIndex >= _scenarioDataList.Count)
@@ -67,40 +67,47 @@ namespace RaraMagi.Systems.TextSystem
 
             //表示される文字数を計算
             int displayCharCount = (int) (Mathf.Clamp01((Time.time - _timeBeganDisplay) / _timeUntilDisplay) *
-                                          _currentLine.Length);
+                                          _currentScenario.Sentence.Length);
             //表示される文字数が表示している文字数と違う
             if (displayCharCount != _lastUpdateCharCount)
             {
-                _parent.SetMainText(_currentLine.Substring(0, displayCharCount));
+                _parent.SetMainText(_currentScenario.Sentence.Substring(0, displayCharCount));
                 //表示している文字数の更新
                 _lastUpdateCharCount = displayCharCount;
             }
         }
 
         // 次の文章をセットする
-        void SetNextLine()
+        private void SetNextLine(bool choice = false, bool yesChoice = false)
         {
-            ScenarioData scenarioData = _scenarioDataList[_currentLineIndex];
+            _currentScenario = _scenarioDataList[_currentLineIndex];
             _parent.SetCharacterImage(
                 CharaImageCreator.Create(
-                    index: scenarioData.CharaImageIndex,
-                    characters: scenarioData.DisplayCharacterName,
-                    charaState: scenarioData.CharaState
+                    index: _currentScenario.CharaImageIndex,
+                    characters: _currentScenario.DisplayCharacterName,
+                    charaState: _currentScenario.CharaState
                 )
             );
 
-            _parent.SetSpeakerText(scenarioData.Speaker);
-            _parent.SetYesChoices(scenarioData.IsBranchChoices, scenarioData.YesChoices);
-            _parent.SetNoChoices(scenarioData.IsBranchChoices, scenarioData.NoChoices);
+            _parent.SetSpeakerText(_currentScenario.Speaker);
+            _parent.SetYesChoices(_currentScenario.IsBranchChoices, _currentScenario.YesChoices);
+            _parent.SetNoChoices(_currentScenario.IsBranchChoices, _currentScenario.NoChoices);
 
-            _currentLine = scenarioData.Sentence;
-            _timeUntilDisplay = _currentLine.Length * intervalForCharDisplay;
+            _timeUntilDisplay = _currentScenario.Sentence.Length * intervalForCharDisplay;
             _timeBeganDisplay = Time.time;
-            _currentLineIndex++;
+            if (_currentScenario.IsSkipSentence) _currentLineIndex = _currentScenario.SkipLine;
+            else if (choice)
+                _currentLineIndex = yesChoice ? _currentScenario.GotoAfterYes : _currentScenario.GotoAfterNo;
+            else _currentLineIndex++;
             _lastUpdateCharCount = 0;
         }
 
-        bool IsCompletedDisplay()
+        private void Choice(bool yes)
+        {
+            SetNextLine(choice: true, yesChoice: yes);
+        }
+
+        private bool IsCompletedDisplay()
         {
             return Time.time > _timeBeganDisplay + _timeUntilDisplay;
         }
