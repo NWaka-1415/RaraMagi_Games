206320ab793c3e2577dd1101908a51e2ef1808f2
NWaka <yuuki.ohta.chinta.p@gmail.com>
2020-06-14 04:58:44 +0900

    :+1: update:先頭行に余分な情報がある際にはじく設定を追加

--- Assets/RaraMagi/Scripts/Systems/TextSystem/TextLoader.cs
+++ Assets/RaraMagi/Scripts/Systems/TextSystem/TextLoader.cs
@@ -7,13 +7,25 @@ namespace RaraMagi.Systems.TextSystem
     {
         private const string TextPath = "Texts";
 
-        public static string[] Load(int chapter, CharacterNames characters)
+        public static string[] Load(int chapter, CharacterNames characters, bool isSkipFirstLine = false)
         {
             TextAsset textAsset = Resources.Load<TextAsset>(
                 $"{TextPath}/{CharacterData.CharaPath[characters]}/{CharacterData.CharaPath[characters]}{chapter}"
             );
             string result = textAsset.text;
-            return result.Replace("\r\n", "\n").Split(new[] {'\n', '\r'});
+            string[] resultArray = result.Replace("\r\n", "\n").Split(new[] {'\n', '\r'});
+            if (isSkipFirstLine)
+            {
+                string[] newResult = new string[resultArray.Length - 1];
+                for (int i = 0; i < newResult.Length; i++)
+                {
+                    newResult[i] = resultArray[i + 1];
+                }
+
+                return newResult;
+            }
+
+            return resultArray;
         }
     }
 }
