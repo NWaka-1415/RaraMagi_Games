1091b6882c662ed3d7c15c6a51453169a9964d2b
NWaka <yuuki.ohta.chinta.p@gmail.com>
2020-06-17 06:27:07 +0900

    :+1: update:変更

--- Assets/RaraMagi/Scripts/Systems/RoomController.cs
+++ Assets/RaraMagi/Scripts/Systems/RoomController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace RaraMagi.Ui
+namespace RaraMagi.Systems
 {
     public class RoomController : MonoBehaviour
     {
