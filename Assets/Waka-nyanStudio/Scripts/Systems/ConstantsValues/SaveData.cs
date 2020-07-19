d90c1de9421ac74dca1e349f408eb8540f2012c9
NWaka <yuuki.ohta.chinta.p@gmail.com>
2020-07-20 08:58:45 +0900

    :+1: update:SaveDataを自動更新

--- Assets/Waka-nyanStudio/Scripts/Systems/ConstantsValues/SaveData.cs
+++ Assets/Waka-nyanStudio/Scripts/Systems/ConstantsValues/SaveData.cs
@@ -1,10 +1,22 @@
-﻿namespace WakanyanStudio.Systems.ConstantsValues
+﻿using RaraMagi.Systems;
+using System.Collections.Generic;
+
+namespace WakanyanStudio.Systems.ConstantsValues
 {
-    /// <summary>
-    /// 保存されるデータ
-    /// </summary>
-    [System.Serializable]
-    public class SaveData
-    {
-    }
+	/// <summary>
+	/// 保存されるデータ
+	/// </summary>
+	[System.Serializable]
+	public class SaveData
+	{
+		public int chapter;
+
+		public CharacterNames chapterChara;
+
+		public SaveData()
+		{
+			chapter = 0;
+			chapterChara = CharacterNames.All;
+		}
+	}
 }
